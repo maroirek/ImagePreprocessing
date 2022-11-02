e1217f791577bd1ed0d30e8d0c4f9dd8b8f983b8
MarouaREKROUK <maroua.rekrouk@g.enp.edu.dz>
2022-11-02 19:46:40 +0100

    Update

--- ACP.m
+++ ACP.m
@@ -1,14 +1,14 @@
-%%%%%%%%%%%%%%%% ACP for matrix data  %%%%%%%%%%%%%%%%%% 
+%%%%%%%%%%%%%%%%%%%%%%%%%%% ACP for matrix data  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
 
 data = [1 2 3 5 6 ;
-    2 3 3 7 8  ;
-    5 6 6 89 9 ;
-    7 7 8 9 8 ];
+        2 3 3 7 8 ;
+        5 6 6 89 9 ;
+        7 7 8 9 8 ];
 
 % 1- Centrage et reduction de la data
 dcr = data;
 for i=1:length(data(1,:))   
- dcr(:,i) = (data(:,i) - mean(data(:,i)))/max(data(:,i));   
+ dcr(:,i) = (data(:,i) - mean(data(:,i)))/std(data(:,i));   
 end
 
 % 2- matrice de covariance
@@ -24,7 +24,7 @@ VecEnd=0;
 LVP=length(ValP);
 for i = LVP :-1 : 1 
     if (inertia<0.5)  % 0.5 = pourcentage d'info qu'on veut garder
-        inertia = inertia + ValP(i);
+        inertia = inertia +(ValP(i))/ sum(ValP);
         VecEnd = i ;   % index of the last vector that will be taken
     end
 end
@@ -34,5 +34,7 @@ V= covar(:, VecEnd: LVP);  % matrice de passage
 % 5- Data reduite en dimension
 
 New_data= data*V;
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% ACP for images %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
